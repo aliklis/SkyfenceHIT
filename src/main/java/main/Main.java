0dbe30283fc3676870ba90ef003b4a079d75eef5
david <frid.david@gmail.com>
2016-09-02 17:36:11 +0300

    added function getRequestList that handle getting the xml files from the system, added proxy loop on the ScenarioManager

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.hit.applications.ApplicationRequest;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 import com.hit.scenarios.ScenarioManager;
 import com.hit.util.*;
 
@@ -12,25 +15,78 @@ public class Main {
 
 	public static void main(String[] args) {
 		String directory = null;
-		List<ApplicationRequest> reqList = new ArrayList<ApplicationRequest>();
+		List<Request> requestList = null;
+
 		if (args.length > 0) {
 			directory = args[0];
 		} else {
 			directory = GetProperties.getProp("defaultXmlDir");
 		}
-		
+
+		requestList = getRequestList(directory);
+
+		if (requestList != null) {
+			for (Request currRequest : requestList) {
+				//if not using proxy set proxy null
+				if(currRequest.getProxies() == null){
+					ScenarioManager.run(currRequest, null);
+				}
+				//run on list of proxies
+				else{
+					List<String> proxyList = currRequest.getProxies();
+					for (String proxy : proxyList) {
+						ScenarioManager.run(currRequest, proxy);
+					}
+				}
+
+			}
+		}
+	}
+
+	/***
+	 * iterate over the files list from given directory(only XML) if no files or
+	 * get an error in process return null else return list of requests
+	 * 
+	 * @param directory
+	 * @return List<Request>
+	 */
+	public static List<Request> getRequestList(String directory) {
+		List<Request> requestList = new ArrayList<Request>();
 		File dir = new File(directory);
 		if (dir.exists() && dir.isDirectory()) {
 			File[] directoryListing = dir.listFiles();
 			if (directoryListing != null) {
 				for (File child : directoryListing) {
-					reqList.add(XMLToRequest.parseXML(child.getPath()));
+					String filename = child.getName();
+					if (filename.endsWith(".xml") || filename.endsWith(".XML")) {
+						try {
+							Request request = getXMLRequest(child.getPath());
+							requestList.add(request);
+						} catch (JAXBException e) {
+							return null;
+						}
+					}
 				}
+				return requestList;
 			}
 		}
-		for(int i=0;i<reqList.size();i++){
-			ScenarioManager.run(reqList.get(i));
-		}
+		return null;
+	}
+
+	/***
+	 * read xml files into request object with JAXB
+	 * 
+	 * @param fileName
+	 * @return
+	 * @throws JAXBException
+	 */
+	public static Request getXMLRequest(String fileName) throws JAXBException {
+		JAXBContext jc = JAXBContext.newInstance(Request.class);
+
+		Unmarshaller unmarshaller = jc.createUnmarshaller();
+		Request resquest = (Request) unmarshaller.unmarshal(new File(fileName));
+		return resquest;
+
 	}
 
 }
